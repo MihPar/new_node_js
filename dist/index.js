849d0984424fa865d3a2df8f3ea2c1787c195f18
MihPar <9496260@gmail.com>
2023-08-22 22:24:42 +0300

    tsc_node2

--- dist/index.js
+++ dist/index.js
@@ -6,19 +6,43 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 4000;
-app.get('/', function (req, res) {
-    const a = 4;
-    if (a > 5) {
-        res.send('OK');
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'automatic qa' },
+        { id: 4, title: 'devops' }
+    ]
+};
+app.get('/courses', function (req, res) {
+    // res.send({message: 'Hello Express Yes!'})
+    // res.send('<h1>Hello Express Yes!!</h1>')
+    // res.json({message: 'Hello Express!'})
+    // res.json('Hello Express!')
+    // res.json(1000)
+    // res.sendStatus(404)
+    let foundCourses = db.courses;
+    if (foundCourses) {
+        foundCourses = foundCourses.filter(function (c) {
+            return c.title.indexOf(req.query.title) > -1;
+        });
     }
-    else {
-        res.send('Hello Express! Yes Yes!!!!');
+    res.json(foundCourses);
+});
+app.get('/courses/:id', function (req, res) {
+    const foundCourses = db.courses.find(c => c.id === Number(req.params.id));
+    if (!foundCourses) {
+        res.sendStatus(404);
+        return;
     }
+    res.json(foundCourses);
 });
-app.get('/samuray', function (req, res) {
-    res.send('Hello Samuray');
+// app.get('/samuray', function(req, res) {
+//     res.send('Hello Samuray')
+// })
+// app.post ('/samuray', function(req, res) {
+//     res.send('We have create Samuray!!!')
+// })
+app.listen(port, '127.0.0.1', function () {
+    console.log(`Server was started at port http://localhost:${port}`);
 });
-app.post('/samuray', function (req, res) {
-    res.send('We have create Samuray!!!');
-});
-app.listen(port, '127.0.0.1', function () { console.log(`Server was started at port http://localhost:${port}`); });
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,46 @@ import express from 'express'
 const app = express()
 const port = 4000
 
-app.get('/', function(req, res) {
-    const a = 4
-    if(a > 5) {
-        res.send('OK')
-    } else {
-        res.send('Hello Express! Yes Yes!!!!')
+const db = {
+    courses: [
+        {id: 1, title: 'front-end'},
+        {id: 2, title: 'back-end'},
+        {id: 3, title: 'automatic qa'},
+        {id: 4, title: 'devops'}
+    ]
+}
+
+app.get('/courses', function(req, res) {
+    // res.send({message: 'Hello Express Yes!'})
+    // res.send('<h1>Hello Express Yes!!</h1>')
+    // res.json({message: 'Hello Express!'})
+    // res.json('Hello Express!')
+    // res.json(1000)
+    // res.sendStatus(404)
+    let foundCourses = db.courses
+    if(foundCourses) {
+        foundCourses = foundCourses.filter(function(c) {
+            return c.title.indexOf(req.query.title as string) > -1
+        })
     }
-})
-app.get('/samuray', function(req, res) {
-    res.send('Hello Samuray')
-})
-app.post ('/samuray', function(req, res) {
-    res.send('We have create Samuray!!!')
+    res.json(foundCourses)
 })
 
-app.listen(port, '127.0.0.1', function() {console.log(`Server was started at port http://localhost:${port}`)})
+app.get('/courses/:id', function(req, res) {
+    const foundCourses = db.courses.find(c => c.id === Number(req.params.id))
+    if(!foundCourses) {
+        res.sendStatus(404)
+        return 
+    }
+    res.json(foundCourses)
+})
+// app.get('/samuray', function(req, res) {
+//     res.send('Hello Samuray')
+// })
+// app.post ('/samuray', function(req, res) {
+//     res.send('We have create Samuray!!!')
+// })
+
+app.listen(port, '127.0.0.1', function() {
+    console.log(`Server was started at port http://localhost:${port}`)
+})
